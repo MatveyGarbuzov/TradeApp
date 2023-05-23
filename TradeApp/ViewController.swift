ae5e57d6a504289bbe61fcb34c28d56c9e074942
MatveyGarbuzov <matveyvoronezh@mail.ru>
2023-05-23 17:59:24 +0300

    First commit

---
+++ TradeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradeApp
+//
+//  Created by Matvey Garbuzov on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
