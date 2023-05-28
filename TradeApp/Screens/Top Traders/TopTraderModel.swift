0e2fe18a5e64e5bf43c98f2dd160a6c5cf291ad2
MatveyGarbuzov <matveyvoronezh@mail.ru>
2023-05-28 19:55:41 +0300

    TopTraderModel add

---
+++ TradeApp/Screens/Top Traders/TopTraderModel.swift
@@ -0,0 +1,17 @@
+//
+//  TopTraderModel.swift
+//  TradeApp
+//
+//  Created by Matvey Garbuzov on 27.05.2023.
+//
+
+import UIKit
+
+struct TopTraderCellModel {
+  let number: Int
+  let country: UIImageView
+  let name: String
+  let deposit: String
+  let profit: String
+}
+
